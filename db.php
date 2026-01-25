1f7afa04e927b26adec8569425696efb0f514a74
jigsuyaaa <jigs02lactao@gmail.com>
2026-01-25 22:34:17 +0800

    change for ur db conn

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_connect("localhost","root","@April192005","ecobench");
+$con = mysqli_connect("localhost","root","Sekaii.","ecobench");
 // Check connection
 if (mysqli_connect_errno())
   {
